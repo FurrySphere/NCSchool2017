0aee5312dfc848a737d4c1fc5ce972d6726a031f
bobaalex <099@tut.by>
2017-10-15 22:42:50 +0300

    Initial commit myProject. Entities of users, subjects and controllers added.

---
+++ AP-Module/myProject/src/main/java/by/nc/school/dev/entities/controllers/AppUserController.java
@@ -0,0 +1,13 @@
+package main.java.by.nc.school.dev.entities.controllers;
+
+import main.java.by.nc.school.dev.entities.users.AppUser;
+import main.java.by.nc.school.dev.entities.users.UserProfile;
+
+import java.util.ArrayList;
+
+/**
+ * Created by User on 15.10.2017.
+ */
+public class AppUserController {
+    ArrayList<AppUser> appUsers;
+}
---
+++ AP-Module/myProject/src/main/java/by/nc/school/dev/entities/users/AppUser.java
@@ -0,0 +1,82 @@
+package main.java.by.nc.school.dev.entities.users;
+
+/**
+ * Created by User on 15.10.2017.
+ */
+public class AppUser {
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    public AppUser(Long id, String username, String password, String role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AppUser appUser = (AppUser) o;
+
+        if (!id.equals(appUser.id)) return false;
+        if (!username.equals(appUser.username)) return false;
+        if (!password.equals(appUser.password)) return false;
+        return role.equals(appUser.role);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + username.hashCode();
+        result = 31 * result + password.hashCode();
+        result = 31 * result + role.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
